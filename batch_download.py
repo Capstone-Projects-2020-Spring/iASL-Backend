6f799842bec84d7117e0c282dc8f136ec62f19ed
Aidan <aidanloza@gmail.com>
2020-02-18 14:35:01 -0500

    added error handling to batch download

--- batch_download.py
+++ batch_download.py
@@ -2,14 +2,12 @@ from pytube import YouTube, Stream
 import json
 
 #a subdirectory named "raw" must exist in the folder this script runs from
-#NOTE: youtube changed their site recently so your pytube library must be slightly different for this to work
-#      ask aidan for the fixed mixins.pyu
 
 #Handling to get list of unique URLS from JSON
 vid_urls = []
 with open('MSASL_train.json', 'r') as f:
     dataset=json.load(f)
-for x in dataset[0:5]: #REMOVE [BOUNDS] FOR WHOLE DATA SET
+for x in dataset[0:100]: #REMOVE [BOUNDS] FOR WHOLE DATA SET
     cut_url = x['url'].split('&', 1)[0]
     if(cut_url not in vid_urls):
         print(cut_url)
@@ -18,6 +16,16 @@ for x in dataset[0:5]: #REMOVE [BOUNDS] FOR WHOLE DATA SET
 print("Number of videos:", len(vid_urls))
 
 #Download all videos from URLs specified in vid_urls
+num_unavailable = 0
+num_downloaded = 0
 for x in vid_urls:
     name = x.split('=')[1]
-    YouTube(url=x).streams.first().download(output_path="raw/", filename=name)
+    try:
+        YouTube(url=x).streams.first().download(output_path="raw/", filename=name)
+        num_downloaded += 1
+    except Exception:
+        print("Video " + name + " is unavailable for download.")
+        num_unavailable += 1
+
+print(str(num_downloaded) + " videos successfully downloaded.")
+print(str(num_unavailable) + " video(s) could not be downloaded.")
