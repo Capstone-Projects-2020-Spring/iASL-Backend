47c997f43bf5288029e7e15d95e21f656e2803a3
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-02-26 17:18:34 -0500

    Add transfer_learning.py (just an outline so far)

---
+++ transfer_learning.py
@@ -0,0 +1,9 @@
+# Read in weights and biases
+
+# Multiply weights and biases by W = 0.7
+
+# Read in new data to train
+
+# Performing training on the new data, updating the above weights and biases
+
+# Save the updated weights and biases
