4f02e400424c7551250673b2f3ac6e95d8a9decc
Tarek <tug35668@temple.edu>
2020-03-18 17:46:19 -0400

    Added multithreading and post processing to realtime detection

--- scripts/realtime-detection.py
+++ scripts/realtime-detection.py
@@ -17,6 +17,8 @@
 import sys
 import os
 import time
+import threading
+import queue
 
 # import keras/tf modules
 #
@@ -24,10 +26,10 @@ import tensorflow as tf
 from keras import backend as K
 K.tensorflow_backend._get_available_gpus()
 from keras.models import model_from_json
+from keras.applications.inception_v3 import preprocess_input
 
 # import image tools
 #
-from PIL import Image
 import numpy as np
 import cv2
 
@@ -63,17 +65,50 @@ INITIAL_DELAY = float(3.0)
 #-----------------------------------------------------------------------------
 
 
-# function: show_statistics
+# function: worker
 #
-# arguments: class_name - the detected letter
-#            confidence - the confidence of the detection
+# arguments: frame_q - the frame queue
+#            score_q - the scores queue
+#            model - the model used for inference
 #
 # return: none
 #
-# This function displays a black screen with the prediction and
-# model confidence
+# This is the function for a worker thread to process
+# an image
 #
-def show_statistics(class_name, confidence):
+def worker(frame_q, score_q, model):
+
+    # run indefinitely
+    #
+    while True:
+
+        # get a frame from the queue
+        #
+        try:
+            frame = frame_q.get()
+        except queue.Empty:
+            continue
+
+        # run through the model
+        #
+        scores = model.predict(frame.reshape(1, 200, 200, 3))
+
+        # put the scores in the scores queue
+        #
+        score_q.put(scores)
+#
+# end of function
+
+
+# function: show_statistics
+#
+# arguments: letters - list of letters to display
+#
+# return: none
+#
+# This function displays a black screen letters predicted
+#
+def show_statistics(letters):
 
     # create a black matrix
     #
@@ -81,16 +116,8 @@ def show_statistics(class_name, confidence):
 
     # put the class_name
     #
-    cv2.putText(text_image, class_name,
+    cv2.putText(text_image, "".join(letters),
                 (30, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1, (255, 255, 255), 2)
-
-
-    # put the confidence as a percentage
-    #
-    cv2.putText(text_image, str(confidence * 100) + "%",
-                (30, 100),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 1, (255, 255, 255), 2)
 
@@ -99,6 +126,49 @@ def show_statistics(class_name, confidence):
     cv2.imshow("Result", text_image)
 #
 # end of function
+
+
+# function: add_letter
+#
+# arguments: letters - list of letters to display
+#            letter - new letter to add
+#
+# return: letters - updated list of letters
+#
+# This is a wrapper function that adds letters to the list
+#
+def add_letter(letters, letter):
+
+    # if this is nothing return original list
+    #
+    if letter == 'nothing':
+        return letters
+
+    # if this is delete, remove last element
+    #
+    elif letter == 'del':
+
+        # check if there is any letters to remove
+        #
+        if len(letters) > 0:
+            letters.pop()
+            return letters
+        return letters
+
+    # add a space if this is space
+    #
+    elif letter == 'space':
+        letters.append(' ')
+        return letters
+
+    # this is a normal letter, append and return
+    #
+    else:
+        letters.append(letter)
+        return letters
+#
+# end of function
+
 
 #-----------------------------------------------------------------------------
 #
@@ -184,6 +254,36 @@ def main(argv):
     # get the current time
     #
     s1 = time.time()
+    index = 0
+
+    # instantiate thread safe queues
+    #
+    frame_q = queue.Queue()
+    score_q = queue.Queue()
+
+    # launch a new worker thread
+    #
+    threading.Thread(target=worker, args=(frame_q, score_q, loaded_model)).start()
+
+    # keep track of the last k predictions
+    #
+    top_k_preds = []
+
+    # keep track of the letters so far
+    #
+    letters = []
+
+    # keep track if we are adding the same letter
+    #
+    same_letter = 0
+
+    # check if this is a delete
+    #
+    is_del = False
+
+    # number of consecutive del predictions
+    #
+    num_del = 0
 
     # loop indefinitely
     #
@@ -260,14 +360,41 @@ def main(argv):
         keypress = cv2.waitKey(1) & 0xFF
         if k == 27: break
 
-        # convert image to grayscale, reshape, and send to model
-        #
-        scores = loaded_model.predict(cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)\
-                                      .reshape(1, 200, 200, 1))
-
-        # display the detection
-        #
-        show_statistics(mapping[np.argmax(scores)], np.max(scores))
+        roi = cv2.resize(roi, (200, 200), interpolation = cv2.INTER_AREA)[:, :, ::-1]
+        roi = preprocess_input(roi)
+        frame_q.put(roi)
+        try:
+            scores = score_q.get()
+            max_ind = np.argmax(scores)
+            max_score = np.max(scores)
+            if len(top_k_preds) < K:
+                top_k_preds.append((max_ind, max_score))
+            elif len(top_k_preds) == K:
+                top_k_preds.pop(0)
+                top_k_preds.append((max_ind, max_score))
+            if len(set([x[0] for x in top_k_preds])) != 1:
+                continue
+            if float(sum([x[1] for x in top_k_preds])) / float(K) < THRESH:
+                continue
+            if mapping[max_ind] == 'del':
+                is_del = True
+                num_del += 1
+            else:
+                is_del = False
+                num_del = 0
+            if(len(letters) == 0):
+                letters = add_letter(letters, mapping[max_ind])
+            elif(mapping[max_ind] == letters[-1] or (mapping[max_ind] == 'space' and letters[-1] == ' ' ) or (num_del > 1)):
+                same_letter += 1
+                if(same_letter == 15 or (is_del and same_letter == 5)):
+                    letters = add_letter(letters, mapping[max_ind])
+                    same_letter = 0                    
+            else:
+                same_letter = 0
+                letters = add_letter(letters, mapping[max_ind])
+        except queue.Empty:
+            continue            
+        show_statistics(letters)            
 
     # exit gracefully
     #
