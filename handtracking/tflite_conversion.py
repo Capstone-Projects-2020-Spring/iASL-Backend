2f25607f54831933f6a3255fa0e32f7e28a56be2
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-04-01 08:38:27 -0400

    Formatting

--- handtracking/tflite_conversion.py
+++ handtracking/tflite_conversion.py
@@ -1,12 +1,10 @@
 # Reads a model trained using TensorFlow
 # Saves the model as a TensorFlow Lite model
 
-#https://www.tensorflow.org/lite/guide/get_started
+# https://www.tensorflow.org/lite/guide/get_started
 
 import tensorflow as tf
 
 converter = tf.lite.TFLiteConverter.from_saved_model(directory_of_saved_model)
-
 tflite_model = converter.convert()
-
 open("converted_model.tflite", "wb").write(tflite_model)
